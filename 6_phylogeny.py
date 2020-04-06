6d29365ccc0f757a40a2b83ecf13526e5178642c
LinguList <mattis.list@posteo.de>
2020-04-06 15:04:39 +0200

    final submission of the data

--- 6_phylogeny.py
+++ 6_phylogeny.py
@@ -1,14 +1,19 @@
 from lingpy.compare.partial import Partial
 from lingpy.convert.plot import plot_tree
 from sys import argv
+from clldutils.text import strip_brackets, split_text
+from collections import defaultdict
+from lingpy import basictypes
 
 if 'all' in argv:
     fname='A_Chen_'
 else:
     fname='D_Chen_'
 
-part = Partial(fname+'partial.tsv')
-part.add_cognate_ids('cogids', 'cogid', idtype='strict')
+part = Partial(fname+'crossids.tsv')
+part.add_cognate_ids('crossids', 'crossid', idtype='strict')
+part.add_entries('cog', 'crossid,concept', lambda x, y: str(x[y[0]])+x[y[1]])
+part.renumber('cog')
 
 part.calculate('distance', ref='cogid')
 part.calculate('tree', tree_calc='neighbor')
@@ -16,6 +21,7 @@ part.calculate('tree', tree_calc='neighbor')
 part.output('dst', filename=fname+'distance')
 part.output('tre', filename=fname+'tree')
 
-plot_tree(str(part.tree), degree=180, filename=fname+'tree')
+if 'plot' in argv:
+    plot_tree(str(part.tree), degree=350, filename=fname+'tree')
 
 
